50ef77495c78339e7b8584fbccd5d4fa9cf38142
Kifk2f <fianyofriedo2@gmail.com>
2026-05-14 17:03:05 +0000

    StatefulWidget Ok

---
+++ lib/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  final String title;
+
+  const HomePage({super.key, required this.title});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+
+}
+
+// Créer l'état de Homepage pour lui permettre de muter
+// State est une clase générique
+class _HomePageState extends State<HomePage>{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+    );
+  }
+}
