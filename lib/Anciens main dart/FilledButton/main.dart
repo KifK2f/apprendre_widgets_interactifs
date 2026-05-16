7a68d32dcd24c065ebbe7ef3d3e4de289c919e99
Kifk2f <fianyofriedo2@gmail.com>
2026-05-16 14:28:07 +0000

    FilledButton Ok

---
+++ lib/Anciens main dart/FilledButton/main.dart
@@ -0,0 +1,28 @@
+import 'package:apprendre_widgets_interactifs/home_page.dart';
+import 'package:flutter/material.dart';
+
+//Un StatefulWidget est un widget avec un état mutable.
+// Il peut changer au fil du temps en réponse aux interactions de l'utilisateur
+// ou aux événements.
+
+//Bouton flottant qui va se metter par dessu du body de noter application
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomePage(title: 'Les widgets Flutter intercatifs'),
+    );
+  }
+}
